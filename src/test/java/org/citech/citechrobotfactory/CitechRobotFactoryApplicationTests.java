b1f73221d1b9f5567410db58d3c7dc76d19e7116
gredezcici <gredezcici@163.com>
2022-05-28 17:40:03 +0800

    actuator

--- src/test/java/org/citech/citechrobotfactory/CitechRobotFactoryApplicationTests.java
+++ src/test/java/org/citech/citechrobotfactory/CitechRobotFactoryApplicationTests.java
@@ -3,7 +3,7 @@ package org.citech.citechrobotfactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {CitechRobotFactoryApplication.class})
 class CitechRobotFactoryApplicationTests {
 
   @Test
